c0a6f5c90d88bea9be7b9dbf81cefb2b2d5303af
quanjingchen <quanjingchen@gmail.com>
2023-03-01 10:42:18 -0800

    set up

---
+++ BurnIt/App.js
@@ -0,0 +1,26 @@
+// Import three components
+// import Summary from './src/components/Summary';
+// import Meal from './src/components/Meal';
+// import Exercise from './src/components/Exercise';
+
+//import styles.js
+// import { styles } from './styles';
+
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+// const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+<View>
+  <Text>Hello World</Text>
+</View>
+  );
+}
+
+
---
+++ BurnIt/firebaseSetup.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBC3ct8Xykxv-pD2-qgBOf41Mq0rF1fBYM",
+  authDomain: "burnit-78c25.firebaseapp.com",
+  projectId: "burnit-78c25",
+  storageBucket: "burnit-78c25.appspot.com",
+  messagingSenderId: "1083588385447",
+  appId: "1:1083588385447:web:0e2c90c99d2d0bb55c9982",
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
+// For more information on how to access Firebase in your project,
+// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
